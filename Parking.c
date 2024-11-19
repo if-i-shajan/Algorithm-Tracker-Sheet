967d44e504c2a3b47b5ca12364eb092a4c541b9f
J. M. Ifthakharul Islam Shajan <jmiishajan2020@gmail.com>
2024-11-19 15:51:20 +0600

    TS-8

---
+++ Parking.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        int dis = (a[n - 1] - a[0]) * 2;
+        printf("%d\n", dis);
+    }
+    return 0;
+}
